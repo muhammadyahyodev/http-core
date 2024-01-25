2241836c0e13d7415d544a319f1a0a36e0cf3666
yakhyo <muhammadyahyodev@gmail.com>
2024-01-25 12:55:11 +0500

    finished CRUD api of product

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -11,29 +11,24 @@ const InternalError = require("../errors/internal.error");
 
 async function getAllProducts(req, res) {
   try {
-    const query = "SELECT * from product";
+    const query = "SELECT * FROM product";
 
     const results = await new Promise((resolve, reject) => {
       pool.query(query, (error, results) => {
-        if(error) {
-          reject(error);
-        } else {
-          resolve(results);
-        };
+        if(error) reject(error)
+        else resolve(results);
       });
-    });
-
-    res.writeHead(200, {
-      "Content-type": "application/json",
     });
 
     const resp = {
       results: results.rows,
     };
 
-    res.end(JSON.stringify(resp));
+    return res
+      .writeHead(200, { "Content-type": "application/json" })
+      .end(JSON.stringify(resp));
   } catch (error) {
-    InternalError(res);
+    return InternalError(res, error);
   }
 };
 
@@ -43,32 +38,26 @@ async function createProduct(req, res) {
       const { title, description } = JSON.parse(data);
 
       if(!title || typeof title !== "string" || typeof description !== "string") {
-        return ValidationError(res);
+        return ValidationError(res, "Validation Error! Give correct data");
       };
 
       const query = 'INSERT INTO product(title, description) VALUES($1, $2)';
-      
-      const new_product = await new Promise((resolve, reject) => {
+      await new Promise((resolve, reject) => {
         pool.query(query, [title, description], (error, result) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(result);
-          };
+          if (error) reject(error)
+          else resolve(result);
         });
-      });
-      
-      res.writeHead(201, {
-        "Content-type": "application/json charset utf-8",
       });
 
       const resp = {
         status: "CREATED"
       };
 
-      res.end(JSON.stringify(resp));
+      return res
+        .writeHead(200, { "Content-type": "application/json" })
+        .end(JSON.stringify(resp));
     } catch (error) {
-      InternalError(res);
+      return InternalError(res);
     }
 };
 
@@ -77,33 +66,27 @@ async function getProductById(req, res) {
     const id = req.url.split("/")[2];
 
     const query = "SELECT * FROM product WHERE id=$1";
-    
     const product = await new Promise((resolve, reject) => {
         pool.query(query, [ id ], (error, result) => {
-          if(error) {
-            reject(error);
-          } else {
-            resolve(result);
-          };
+          if (error) reject(error)
+          else resolve(result);
         });
     });
 
     if(product.length < 1) {
-      return NotFoundError(res, 'No product found with this id');
+      return NotFoundError(res, 'Not Found! Product cannot be found');
     };
-
-    res.writeHead(200, {
-      "Content-type": "application/json charset utf-8",
-    });
 
     const resp = {
       status: 200,
       product: product.rows,
     };
 
-    res.end(JSON.stringify(resp));
+    return res
+      .writeHead(200, { "Content-type": "application/json" })
+      .end(JSON.stringify(resp));
   } catch (error) {
-    InternalError(res);
+    return InternalError(res);
   }
 };
 
@@ -112,32 +95,19 @@ async function updateProductById(req, res) {
     const id = req.url.split("/")[2];
 
     const check = await checkNotFound(id);
-
     if(check.rows.length < 1) {
-      return NotFoundError(res, 'With id product not found');
+      return NotFoundError(res, 'Not Found! Cannot be found');
     };
 
     const body = await getBodyData(req);
-    
     const { title, description } = JSON.parse(body);
     
     const query = `UPDATE product SET title=$1, description=$2 WHERE id=2`;
-    
-    const values = [ title, description ];
-
     await new Promise((resolve, reject) => {
-      pool.query(query, values, (error, result) => {
-        if(error){
-          console.log(error);
-          reject(error);
-        } else {
-          resolve(result);
-        };
+      pool.query(query, [ title, description ], (error, result) => {
+        if (error) reject(error)
+        else resolve(result);
       });
-    });
-
-    res.writeHead(200, {
-      "Content-type": "application/json charset utf-8",
     });
 
     const resp = {
@@ -145,9 +115,11 @@ async function updateProductById(req, res) {
       message: "UPDATED",
     };
 
-    res.end(JSON.stringify(resp));
+    return res
+      .writeHead(200, { "Content-type": "application/json" })
+      .end(JSON.stringify(resp));
   } catch (error) {
-    InternalError(res);
+    return InternalError(res);
   };
 };
 
@@ -156,25 +128,16 @@ async function deleteProductById(req, res) {
     const id = req.url.split("/")[2];
 
     const check = await checkNotFound(id);
-
     if(check.rows.length < 1) {
-      return NotFoundError(res, 'With id product not found');
+      return NotFoundError(res, 'Not Found! Cannot be found');
     };
     
     const query = "DELETE FROM product WHERE id=$1";
-    
     await new Promise((resolve, reject) => {
       pool.query(query, [ id ], (error, result) => {
-        if(error){
-          reject(error);
-        } else {
-          resolve(result);
-        };
+        if (error) reject(error)
+        else resolve(result);
       });
-    });
-    
-    res.writeHead(200, {
-      "Content-type": "application/json charset utf-8",
     });
 
     const resp = {
@@ -182,29 +145,27 @@ async function deleteProductById(req, res) {
       message: "DELETED",
     };
 
-    res.end(JSON.stringify(resp));
+    return res
+      .writeHead(200, { "Content-type": "application/json" })
+      .end(JSON.stringify(resp));
   } catch (error) {
-    InternalError(res);
+    return InternalError(res);
   };
 };
 
 async function checkNotFound(id) {
   try {
     const query = 'SELECT * FROM product WHERE id=$1';
-    
     const result = await new Promise((resolve, reject) => {
       pool.query(query, [ id ], (error,result) => {
-        if(error){
-          reject(error);
-        } else {
-          resolve(result);
-        };
+        if (error) reject(error)
+        else resolve(result);
       });
     });
 
     return result;
   } catch(error) {
-    InternalError(error);
+    return InternalError(error);
   };
 };
 
